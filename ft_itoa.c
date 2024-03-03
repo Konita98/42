bcfdbbf55deff9cc0322bfff62fd379955c31551
Nahintsara Erica Kone <nerica-k@c1r6s15.42antananarivo.mg>
2024-03-03 18:04:31 +0300

    itoa is good

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,29 +1,58 @@
-char *itoa(int n);
-char *ft_itoa(int n)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nerica-k <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/03 18:02:31 by nerica-k          #+#    #+#             */
+/*   Updated: 2024/03/03 18:02:56 by nerica-k         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	abs(int n)
+{
+	if (n < 0)
+		return (n * (-1));
+	return (n);
+}
+
+char	*ft_itoa(int n)
 {
 	char	*result;
-	int	ch;
-	int	d;
+	int		i;
+	int		nb;
 
-	ch = 1;
-	d = 1;
-	while (n / 10 > 10)
-	result = (char *)malloc(ch * sizeof(char));
-		ch++;
-	if (result == 0)
-		return (NULL);
-	if (nb < 0)
-	{
-		result[i] = '-';
+	i = 1;
+	nb = abs(n);
+	if (n == -2147483648)
+		return ("-2147483648");
+	if (n < 0)
 		i++;
-	}
-	if (nb < 9)
-		result[i] = nb + '0';
-	while (ch != 0)
-		d *= 10;
-		ch--;
-	while (nb > 9 && i <)
+	while (nb >= 10)
 	{
-		result[i]
+		i++;
+		nb /= 10;
 	}
+	result = (char *)malloc((i) * sizeof(char));
+	if (n < 0)
+		result[0] = 45;
+	result[i] = '\0';
+	while (i-- > 1)
+	{
+		result[i] = (abs(n) % 10) + '0';
+		n /= 10;
+	}
+	return (result);
 }
+/*
+#include <stdio.h>
+int main(void)
+{
+	int i;
+	i = -21474865;
+	printf("ft existant %s\n",ft_itoa(i));
+	return (0);
+}*/
